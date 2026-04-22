931d228671ac6897a0786beae5dd3c13fc293197
sachin singh <sachinsingh22443@gmail.com>
2026-04-22 10:46:16 +0530

    solve

--- app/db/session.py
+++ app/db/session.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.api.deps import get_db
 
 load_dotenv()  # 🔥 sabse pehle
 
@@ -14,13 +15,6 @@ engine = create_engine(
 )
 
 
-# postgres
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# engine = create_engine(
-#     DATABASE_URL
-#     # connect_args={"sslmode": "require"}
-# )
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
