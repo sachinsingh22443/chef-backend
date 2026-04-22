9a7ca08966c870f3199735383ba972feeda61b66
sachin singh <sachinsingh22443@gmail.com>
2026-04-22 10:27:32 +0530

    fixes

--- app/db/session.py
+++ app/db/session.py
@@ -6,21 +6,21 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()  # 🔥 sabse pehle
 
 # render database
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# engine = create_engine(
-#     DATABASE_URL,
-#     connect_args={"sslmode": "require"}
-# )
-
-
-# postgres
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL
-    # connect_args={"sslmode": "require"}
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
 )
+
+
+# postgres
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# engine = create_engine(
+#     DATABASE_URL
+#     # connect_args={"sslmode": "require"}
+# )
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
